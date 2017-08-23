5e0c8217cfc648f8a32d2dd8506cdd0547f70973
LucasSimon <lucas.oscar.simon@gmail.com>
2017-08-22 23:29:36 -0300

    Two possible solutions to the same problem

--- CountingMinorOrEqualNumbersBetweenArrays/Program.cs
+++ CountingMinorOrEqualNumbersBetweenArrays/Program.cs
@@ -10,6 +10,16 @@ namespace CountingMinorOrEqualNumbersBetweenArrays
     {
         static void Main(string[] args)
         {
+            string[] numsStr = Console.ReadLine().Split(',');
+            string[] maxesStr = Console.ReadLine().Split(',');
+
+            int[] nums = numsStr.Select(int.Parse).ToArray();
+            int[] maxes = maxesStr.Select(int.Parse).ToArray();
+
+            var result = maxes.Select(m => nums.Count(n => (n <= m))).ToArray();
+
+            Console.WriteLine(String.Join(",", result));
+            Console.ReadLine();
         }
     }
 }
---
+++ CountingMinorOrEqualNumbersBetweenArrays/SolutionTwo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingMinorOrEqualNumbersBetweenArrays
+{
+    class SolutionTwo
+    {
+        static int[] CountNumbers(int[] nums, int[] maxes)
+        {
+            var result = new int[maxes.Count()];
+            for (int max = 0; max < maxes.Count(); max++)
+            {
+                for (int num = 0; num < nums.Count(); num++)
+                {
+                    if (nums[num] <= maxes[max])
+                        result[max] = result[max] + 1;
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] nums = Console.ReadLine().Split(' ');
+            string[] maxes = Console.ReadLine().Split(' ');
+
+            int[] numsArr = nums.Select(int.Parse).ToArray();
+            int[] maxesArr = maxes.Select(int.Parse).ToArray();
+
+            var result = CountNumbers(numsArr, maxesArr);
+
+            Console.WriteLine(String.Join(" ", result));
+            Console.ReadLine();
+        }
+    }
+}
